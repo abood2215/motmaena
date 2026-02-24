960bc8174806b25cd44e2bad2dd66716670659b8
abdallah alawneh <abdallahalawneh013@gmail.com>
2026-02-25 00:50:44 +0300

    docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,6 @@ php artisan config:cache || true
 php artisan route:cache  || true
 php artisan view:cache   || true
 php artisan migrate --force || true
+php artisan db:seed --force || true
 
 exec "$@"
